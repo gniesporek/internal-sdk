f1103a11f1c2607e3bfe04cfe66b1f5657c472a3
ogpolak <niesporek1234@gmail.com>
2026-07-05 16:59:14 +0200

    memory/logging

--- core/utils/utils.cpp
+++ core/utils/utils.cpp
@@ -5,6 +5,8 @@ void Utils::Console::attach()
     AllocConsole();
     freopen_s(&file, "CONOUT$", "w", stdout);
     SetConsoleTitleA("command line");
+
+	MESSAGE_SUCCESS("console attached");
 }
 void Utils::Console::destroy()
 {
@@ -15,4 +17,121 @@ void Utils::Console::destroy()
     }
 
     FreeConsole();
+}
+
+
+void Utils::Console::log_to_console(ConsoleColor color, const char* prefix, const char* file, int line, const char* format, ...)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, (WORD)color);
+
+	std::cout << prefix;
+
+	SetConsoleTextAttribute(hConsole, (WORD)ConsoleColor::Default);
+
+	char buffer[1024];
+
+	va_list args;
+	va_start(args, format);
+	vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, format, args);
+	va_end(args);
+
+	std::cout << buffer;
+
+	const char* filename = strrchr(file, '\\');
+	if (!filename) filename = strrchr(file, '/');
+	filename = filename ? filename + 1 : file;
+
+	SetConsoleTextAttribute(hConsole, (WORD)ConsoleColor::FileLine);
+	std::cout << " (" << filename << ":" << line << ")";
+
+	SetConsoleTextAttribute(hConsole, (WORD)ConsoleColor::Default);
+	std::cout << std::endl;
+}
+
+void* Utils::Memory::get_virtual_function(void* class_base, std::size_t index)
+{
+	void** vtable = *reinterpret_cast<void***>(class_base);
+	return vtable[index];
+}
+
+uint8_t* Utils::Memory::signature_scan(const char* moduleName, const char* pattern)
+{
+	HMODULE module_handle = GetModuleHandle(moduleName);
+
+	if (!module_handle)
+	{
+		return nullptr;
+	}
+
+	PIMAGE_DOS_HEADER dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(module_handle);
+	PIMAGE_NT_HEADERS nt_headers = reinterpret_cast<PIMAGE_NT_HEADERS>(reinterpret_cast<std::uint8_t*>(module_handle) + dos_header->e_lfanew);
+
+	std::size_t size_of_image = nt_headers->OptionalHeader.SizeOfImage;
+	std::vector<std::uint32_t> pattern_bytes = signature_to_byte(pattern);
+	std::uint8_t* image_base = reinterpret_cast<std::uint8_t*>(module_handle);
+
+	std::size_t pattern_size = pattern_bytes.size();
+	std::uint32_t* array_of_bytes = pattern_bytes.data();
+
+	for (std::size_t i = 0; i < size_of_image - pattern_size; i++)
+	{
+		bool found = true;
+
+		for (std::size_t j = 0; j < pattern_size; j++)
+		{
+			if (image_base[i + j] != array_of_bytes[j] && array_of_bytes[j] != -1)
+			{
+				found = false;
+				break;
+			}
+		}
+
+		if (found)
+		{
+			return &image_base[i];
+		}
+	}
+
+	return nullptr;
+}
+
+uint8_t* Utils::Memory::relative_address(uint8_t* address, std::ptrdiff_t offset, std::ptrdiff_t instruction_size)
+{
+	if (!address)
+		return nullptr;
+
+	int32_t rel = *reinterpret_cast<int32_t*>(address + offset);
+
+	uintptr_t rip = reinterpret_cast<uintptr_t>(address) + instruction_size;
+
+	return reinterpret_cast<uint8_t*>(rip + rel);
+}
+
+std::vector<std::uint32_t>  Utils::Memory::signature_to_byte(const char* pattern)
+{
+	std::vector<std::uint32_t> bytes;
+	char* start = const_cast<char*>(pattern);
+	char* end = const_cast<char*>(pattern) + std::strlen(pattern);
+
+	for (char* current = start; current < end; current++)
+	{
+		if (*current == '?')
+		{
+			current++;
+
+			if (*current == '?')
+			{
+				current++;
+			}
+
+			bytes.push_back(-1);
+		}
+		else
+		{
+			bytes.push_back(std::strtoul(current, &current, 16));
+		}
+	}
+
+	return bytes;
 }
--- core/utils/utils.h
+++ core/utils/utils.h
@@ -3,6 +3,20 @@
 #include <iostream>
 #include <Windows.h>
 #include <thread>
+#include <vector>
+
+enum class ConsoleColor : WORD {
+    Default = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+    Info = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+    Success = FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+    Error = FOREGROUND_RED | FOREGROUND_INTENSITY,
+    FileLine = FOREGROUND_RED | FOREGROUND_GREEN
+};
+
+#define MESSAGE(...) utils.console.log_to_console(ConsoleColor::Default,"[LOG] ",__FILE__,__LINE__,__VA_ARGS__);
+#define MESSAGE_INFO(...) utils.console.log_to_console(ConsoleColor::Info,"[INFO] ",__FILE__,__LINE__,__VA_ARGS__);
+#define MESSAGE_SUCCESS(...) utils.console.log_to_console(ConsoleColor::Success,"[SUCCESS] ",__FILE__,__LINE__,__VA_ARGS__);
+#define MESSAGE_ERROR(...) utils.console.log_to_console(ConsoleColor::Error,"[ERROR] ",__FILE__,__LINE__,__VA_ARGS__);
 
 class Utils {
 public:
@@ -10,11 +24,28 @@ public:
     public:
         void attach();
         void destroy();
+		void log_to_console(ConsoleColor color, const char* prefix, const char* file, int line, const char* format, ...);
     private:
         FILE* file = nullptr;
     };
 
-    Console console;
-};
 
+    class Memory {
+    public:
+        template <typename T, typename ... U>
+        T call_virtual_function(void* thisptr, const size_t index, U ... params)
+        {
+            typedef T(__thiscall* Fn)(void*, decltype(params)...);
+            return (*static_cast<Fn**>(thisptr))[index](thisptr, params...);
+        }
+        void* get_virtual_function(void* class_base, std::size_t index);
+        uint8_t* signature_scan(const char* moduleName, const char* pattern);
+        uint8_t* relative_address(uint8_t* address, std::ptrdiff_t offset, std::ptrdiff_t instruction_size);
+    private:
+        std::vector<std::uint32_t> signature_to_byte(const char* pattern);
+    };
+
+    Console console;
+    Memory memory;
+};
 inline Utils utils;
