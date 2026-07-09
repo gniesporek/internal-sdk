c3a42ed86ad8f98257dd4dfc80ab439876055895
ogpolak <niesporek1234@gmail.com>
2026-07-09 21:50:28 +0200

    update signature

--- core/hooks/hooks.cpp
+++ core/hooks/hooks.cpp
@@ -10,7 +10,8 @@ bool HooksManager::Setup()
     CreateHook("PresentScene", "gameoverlayRenderer64.dll", "48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 54 41 56 41 57 48 83 EC ? 41 8B E8", false, &PresentScene::oPresentScene, PresentScene::hkPresentScene);
     CreateHook("CreateSwapChain", "gameoverlayRenderer64.dll", "40 53 55 56 57 48 83 EC ? 48 8B F9 49 8B F1 48 8D 0D ? ? ? ? 49 8B D8 48 8B EA E8 ? ? ? ? 48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8B 05 ? ? ? ? 4C 8B CE 4C 8B C3 48 8B D5 48 8B CF FF D0 8B D8 85 C0 78 ? 48 85 F6 74 ? 48 83 3E ? 74 ? 48 8B D5 48 8B CE E8 ? ? ? ? 8B C3 48 83 C4 ? 5F 5E 5D 5B C3 CC CC CC CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC", false, &CreateSwapChain::oCreateSwapChain, CreateSwapChain::hkCreateSwapChain);
     CreateHook("ResizeBuffers", "gameoverlayRenderer64.dll", "40 53 55 56 57 41 54 41 56 41 57 48 83 EC ? 44 8B E2", false, &ResizeBuffers::oResizeBuffers, ResizeBuffers::hkResizeBuffers);
-    CreateHook("OnClientOutput", "engine2.dll", "48 89 5C 24 ? 55 56 57 41 56 41 57 48 83 EC ? 48 8D 05", false, &OnClientOutput::oOnClientOutput, OnClientOutput::hkOnClientOutput);
+    CreateHook("OnClientOutput", "engine2.dll", "4C 8B DC 49 89 5B ? 49 89 6B ? 49 89 73 ? 57 41 56 41 57 48 83 EC", false, &OnClientOutput::oOnClientOutput, OnClientOutput::hkOnClientOutput);
+
     CreateHook("RelativeMouseMode", "inputsystem.dll", "48 89 6C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 41 56 48 83 EC ? 0F B6 F2", false, &RelativeMouseMode::oSetRelativeMouseMode, RelativeMouseMode::hkSetRelativeMouseMode);
     CreateHook("MouseInput", "client.dll", "40 53 48 83 EC ? 80 B9 ? ? ? ? ? 48 8B D9 75 ? 48 8B 0D ? ? ? ? 48 8B 01", false, &MouseInput::oMouseInput, MouseInput::hkMouseInput);
 
